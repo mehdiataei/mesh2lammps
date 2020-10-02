dcc1ce4f1fe87da1c94ed7feaf6f9ef2a4a73522
perfan <56326009+perfan@users.noreply.github.com>
2020-10-01 21:03:56 -0400

    packaged the python scripts to CLS and tested

--- src/lammps2mesh.py
+++ src/lammps2mesh.py
@@ -4,49 +4,50 @@ import os
 import sys
 import pandas as pd
 
+def main():
 
-atoms = pd.read_csv('../models/'+sys.argv[1], delimiter=' ')
-angles = pd.read_csv('../models/'+sys.argv[2], delimiter=' ')
+    atoms = pd.read_csv( sys.argv[1], delimiter=' ')
+    angles = pd.read_csv( sys.argv[2], delimiter=' ')
 
-vertices = []
-for index, row in atoms.iterrows():
-    vertices += [[row['x'], row['y'], row['z']]]
-faces = []
-for index, row in angles.iterrows():
-    faces += [[row['v0'] - 1, row['v1'] - 1, row['v2'] - 1]]
+    vertices = []
+    for index, row in atoms.iterrows():
+        vertices += [[row['x'], row['y'], row['z']]]
+    faces = []
+    for index, row in angles.iterrows():
+        faces += [[row['v0'] - 1, row['v1'] - 1, row['v2'] - 1]]
 
-mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+    mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
 
-maxz = np.where(mesh.vertices[:,2] == mesh.vertices[:,2].max()) 
-minz = np.where(mesh.vertices[:,2] == mesh.vertices[:,2].min()) 
+    maxz = np.where(mesh.vertices[:,2] == mesh.vertices[:,2].max()) 
+    minz = np.where(mesh.vertices[:,2] == mesh.vertices[:,2].min()) 
 
-maxy = np.where(mesh.vertices[:,1] == mesh.vertices[:,1].max()) 
-miny = np.where(mesh.vertices[:,1] == mesh.vertices[:,1].min()) 
+    maxy = np.where(mesh.vertices[:,1] == mesh.vertices[:,1].max()) 
+    miny = np.where(mesh.vertices[:,1] == mesh.vertices[:,1].min()) 
 
-if(mesh.is_watertight):
-    print("Volumetric center of mass is set as the origin for the mesh")
-    mesh.vertices -= mesh.center_mass
+    if(mesh.is_watertight):
+        print("Volumetric center of mass is set as the origin for the mesh")
+        mesh.vertices -= mesh.center_mass
 
-maxx = np.where(mesh.vertices[:,0] == mesh.vertices[:,0].max()) 
-minx = np.where(mesh.vertices[:,0] == mesh.vertices[:,0].min())
+    maxx = np.where(mesh.vertices[:,0] == mesh.vertices[:,0].max()) 
+    minx = np.where(mesh.vertices[:,0] == mesh.vertices[:,0].min())
 
-maxy = np.where(mesh.vertices[:,1] == mesh.vertices[:,1].max()) 
-miny = np.where(mesh.vertices[:,1] == mesh.vertices[:,1].min())
+    maxy = np.where(mesh.vertices[:,1] == mesh.vertices[:,1].max()) 
+    miny = np.where(mesh.vertices[:,1] == mesh.vertices[:,1].min())
 
-maxz = np.where(mesh.vertices[:,2] == mesh.vertices[:,2].max()) 
-minz = np.where(mesh.vertices[:,2] == mesh.vertices[:,2].min())
+    maxz = np.where(mesh.vertices[:,2] == mesh.vertices[:,2].max()) 
+    minz = np.where(mesh.vertices[:,2] == mesh.vertices[:,2].min())
 
-tanXY = np.abs(mesh.vertices[maxx][0][0] - mesh.vertices[minx][0][0] / 2) / np.abs(mesh.vertices[maxx][0][1] - mesh.vertices[minx][0][1] / 2)
-tanXZ = np.abs(mesh.vertices[maxz][0][0] - mesh.vertices[minz][0][0] / 2) / np.abs(mesh.vertices[maxz][0][2] - mesh.vertices[minz][0][2] / 2)
+    tanXY = np.abs(mesh.vertices[maxx][0][0] - mesh.vertices[minx][0][0] / 2) / np.abs(mesh.vertices[maxx][0][1] - mesh.vertices[minx][0][1] / 2)
+    tanXZ = np.abs(mesh.vertices[maxz][0][0] - mesh.vertices[minz][0][0] / 2) / np.abs(mesh.vertices[maxz][0][2] - mesh.vertices[minz][0][2] / 2)
 
-rot_matrixXY = trimesh.transformations.rotation_matrix(np.arctan(tanXY) / 2, [0, 0, 1], [0, 0, 0])
-rot_matrixXZ = trimesh.transformations.rotation_matrix(np.arctan(tanXZ), [0, 1, 0], [0, 0, 0])
+    rot_matrixXY = trimesh.transformations.rotation_matrix(np.arctan(tanXY) / 2, [0, 0, 1], [0, 0, 0])
+    rot_matrixXZ = trimesh.transformations.rotation_matrix(np.arctan(tanXZ), [0, 1, 0], [0, 0, 0])
 
-mesh.vertices = trimesh.transformations.transform_points(mesh.vertices, rot_matrixXY)
-mesh.vertices = trimesh.transformations.transform_points(mesh.vertices, rot_matrixXZ)
+    mesh.vertices = trimesh.transformations.transform_points(mesh.vertices, rot_matrixXY)
+    mesh.vertices = trimesh.transformations.transform_points(mesh.vertices, rot_matrixXZ)
 
-mesh.fix_normals()
-mesh.export('centered_mesh.stl')
-print("Mesh file is generated!")
+    mesh.fix_normals()
+    mesh.export('centered_mesh.stl')
+    print("Mesh file is generated!")
 
-mesh.show()
+    mesh.show()
